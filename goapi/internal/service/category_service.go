5e5956e0c16267fb9aeb436e097ffec1f7406c7d
silva4dev <web4dev.lucas@outlook.com>
2024-04-26 18:59:32 -0300

    feat: add category service

---
+++ goapi/internal/service/category_service.go
@@ -0,0 +1,39 @@
+package service
+
+import (
+	"github.com/silva4dev/ecommerce-ms-project/internal/database"
+	"github.com/silva4dev/ecommerce-ms-project/internal/entity"
+)
+
+type CategoryService struct {
+	CategoryDB database.CategoryDB
+}
+
+func NewCategoryService(categoryDB database.CategoryDB) *CategoryService {
+	return &CategoryService{CategoryDB: categoryDB}
+}
+
+func (cs *CategoryService) GetCategories() ([]*entity.Category, error) {
+	categories, err := cs.CategoryDB.GetCategories()
+	if err != nil {
+		return nil, err
+	}
+	return categories, nil
+}
+
+func (cs *CategoryService) CreateCategory(name string) (*entity.Category, error) {
+	category := entity.NewCategory(name)
+	_, err := cs.CategoryDB.CreateCategory(category)
+	if err != nil {
+		return nil, err
+	}
+	return category, nil
+}
+
+func (cs *CategoryService) GetCategoryByID(id string) (*entity.Category, error) {
+	category, err := cs.CategoryDB.GetCategoryByID(id)
+	if err != nil {
+		return nil, err
+	}
+	return category, nil
+}
