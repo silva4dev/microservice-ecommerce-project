b1dad4761650da188746869c7c0079236b1ab808
silva4dev <web4dev.lucas@outlook.com>
2024-04-26 18:58:50 -0300

    feat: add main

---
+++ goapi/cmd/catalog/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/silva4dev/ecommerce-ms-project/internal/database"
+	"github.com/silva4dev/ecommerce-ms-project/internal/service"
+	"github.com/silva4dev/ecommerce-ms-project/internal/webserver"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/goapi")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	categoryDB := database.NewCategoryDB(db)
+	categoryService := service.NewCategoryService(*categoryDB)
+
+	productDB := database.NewProductDB(db)
+	productService := service.NewProductService(*productDB)
+
+	webCategoryHandler := webserver.NewWebCategoryHandler(categoryService)
+	webProductHandler := webserver.NewWebProductHandler(productService)
+
+	c := chi.NewRouter()
+	c.Use(middleware.Logger)
+	c.Use(middleware.Recoverer)
+
+	c.Get("/category/{id}", webCategoryHandler.GetCategoryByID)
+	c.Get("/category", webCategoryHandler.GetCategories)
+	c.Post("/category", webCategoryHandler.CreateCategory)
+
+	c.Get("/product/{id}", webProductHandler.GetProductByID)
+	c.Get("/product", webProductHandler.GetProducts)
+	c.Get("/product/category/{categoryID}", webProductHandler.GetProductByCategoryID)
+	c.Post("/product", webProductHandler.CreateProduct)
+
+	fmt.Println("Server is running on port 8080")
+	http.ListenAndServe(":8080", c)
+}
