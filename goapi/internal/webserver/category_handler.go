ab700afc98c995b9d8c9b4a002fa323ef912bb35
silva4dev <web4dev.lucas@outlook.com>
2024-04-26 18:59:59 -0300

    feat: add category handler

---
+++ goapi/internal/webserver/category_handler.go
@@ -0,0 +1,56 @@
+package webserver
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/silva4dev/ecommerce-ms-project/internal/entity"
+	"github.com/silva4dev/ecommerce-ms-project/internal/service"
+)
+
+type WebCategoryHandler struct {
+	CategoryService *service.CategoryService
+}
+
+func NewWebCategoryHandler(categoryService *service.CategoryService) *WebCategoryHandler {
+	return &WebCategoryHandler{CategoryService: categoryService}
+}
+
+func (wch *WebCategoryHandler) GetCategories(w http.ResponseWriter, r *http.Request) {
+	categories, err := wch.CategoryService.GetCategories()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(categories)
+}
+
+func (wch *WebCategoryHandler) GetCategoryByID(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		http.Error(w, "id is required", http.StatusBadRequest)
+		return
+	}
+	category, err := wch.CategoryService.GetCategoryByID(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(category)
+}
+
+func (wch *WebCategoryHandler) CreateCategory(w http.ResponseWriter, r *http.Request) {
+	var category entity.Category
+	err := json.NewDecoder(r.Body).Decode(&category)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	result, err := wch.CategoryService.CreateCategory(category.Name)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(result)
+}
