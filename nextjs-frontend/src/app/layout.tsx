af06f7593f7d9ad0fdce5a55d3d291c627fe2c60
silva4dev <web4dev.lucas@outlook.com>
2024-05-30 20:42:39 -0300

    feat: add layout

--- nextjs-frontend/src/app/layout.tsx
+++ nextjs-frontend/src/app/layout.tsx
@@ -3,20 +3,20 @@ import { Inter } from "next/font/google";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import { Box, ThemeProvider } from "@mui/material";
 import theme from "../theme";
-import { Navbar } from "@/components/Navbar/Navbar";
+import { Navbar } from "../components/Navbar/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Code Commerce",
-  description: "Uma full cycle shop",
+  description: "Uma Full Cycle shop",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body className={inter.className}>
