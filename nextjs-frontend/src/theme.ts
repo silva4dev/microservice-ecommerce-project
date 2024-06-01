016dff85f3ef741be9fbfc43abbb0c3cb5116aa9
silva4dev <web4dev.lucas@outlook.com>
2024-06-01 20:39:54 -0300

    feat: add theme

--- nextjs-frontend/src/theme.ts
+++ nextjs-frontend/src/theme.ts
@@ -1,21 +1,21 @@
-"use client";
+'use client';
 
-import { Roboto } from "next/font/google";
-import { createTheme } from "@mui/material/styles";
+import { Roboto } from 'next/font/google';
+import { createTheme } from '@mui/material/styles';
 
 const roboto = Roboto({
-  weight: ["300", "400", "500", "700"],
-  subsets: ["latin"],
-  display: "swap",
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
 });
 
 const theme = createTheme({
   palette: {
-    mode: "light",
+    mode: 'light',
   },
   typography: {
     fontFamily: roboto.style.fontFamily,
-  },
+  }
 });
 
 export default theme;
